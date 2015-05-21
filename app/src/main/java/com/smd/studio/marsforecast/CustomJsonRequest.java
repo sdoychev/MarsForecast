65b5f7014a8e08b427d15eb87593eec370b06adf
Stefan Doychev <sdoychev@gmail.com>
2015-05-22 01:36:03 +0300

    Add class for the CustomJsonRequests.

---
+++ app/src/main/java/com/smd/studio/marsforecast/CustomJsonRequest.java
@@ -0,0 +1,29 @@
+package com.smd.studio.marsforecast;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Doychev on 22.5.2015 ã..
+ */
+public class CustomJsonRequest extends JsonObjectRequest {
+
+    private Priority mPriority;
+
+    public CustomJsonRequest(int method, String url, JSONObject jsonRequest,
+                             Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        super(method, url, jsonRequest, listener, errorListener);
+    }
+
+    @Override
+    public Priority getPriority() {
+        return mPriority == null ? Priority.NORMAL : mPriority;
+    }
+
+    public void setPriority(Priority priority) {
+        mPriority = priority;
+    }
+
+}
